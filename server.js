2f92dddc49c1b5d489e576662f0172c267832491
sharmila251995 <manubolusaisharmila@gmail.com>
2020-09-14 21:41:14 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 8080
+const server = require('http').Server(app);
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + 'dist', {index: false}));
 
-app.listen(process.env.PORT || 8080);
+server.listen(port, function() {
+    console.log("App running on port " + port);
+})
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname ,'src', 'index.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname , 'src', 'index.html'));
 })
 
 console.log('Console listening!');
