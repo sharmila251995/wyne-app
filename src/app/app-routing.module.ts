9bb6727dcd46a89f8afb7ff3f56fe1651e231805
sharmila251995 <manubolusaisharmila@gmail.com>
2020-09-14 19:42:07 +0530

    Initial app commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewUserComponent } from './new-user/new-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'new-user', component: NewUserComponent },
+  { path: '',   redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
